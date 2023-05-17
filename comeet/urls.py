7e0791056affb60dcb5e995de85d11a9aefa8aeb
VinylStage <mcpe9869@gmail.com>
2023-05-17 18:50:39 +0900

    project init

--- comeet/urls.py
+++ comeet/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user.urls')),
-    path('', include('commit.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("user.urls")),
+    path("", include("commit.urls")),
+    path("", include("django_nextjs.urls")),
 ]
